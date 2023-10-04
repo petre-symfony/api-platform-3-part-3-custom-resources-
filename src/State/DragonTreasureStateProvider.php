0a732cb6cc4365b12a812a33a747e5e034e195b4
Petre Ro <petrero@gmail.com>
2023-10-04 10:55:34 +0300

    3.3 Decorating the Provider  - But... I don't want to do all the work of querying the database for the dragon treasures... because there's already a core entity provider that does all that! So let's use it!
     Add a constructor... oh and I'll keep that dd() for now. Add a private ProviderInterface $itemProvider argument.
    
     As a reminder: the Get one, Patch, Put and Delete operations all use the ItemProvider, which knows to query for a single item. Since our test uses Patch, we're going to focus on using that provider first.
    
     If we run the test now, it fails. The error is:
    
      Cannot autowire service DragonTreasureStateProvider: argument itemProvider references ProviderInterface, but no such service exists.

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -6,6 +6,9 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 
 class DragonTreasureStateProvider implements ProviderInterface {
+	public function __construct(private ProviderInterface $itemProvider){
+
+	}
 	public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
 		dd($operation);
 	}
