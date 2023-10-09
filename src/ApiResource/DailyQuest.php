9eadfab1d3508e47a029db88673cbc47fb68baba
Petre Ro <petrero@gmail.com>
2023-10-09 21:13:29 +0300

    9.5 Though, now we have a dayString field in the JSON... as well as the day itself. Let's think. We really don't need the day field at all: it exists internally just to help the dayString. And because the dayString is in the URL, having that as a field also seems unnecessary. Can we hide these?
     Hiding Specific Fields from your API
     - Sure! And we don't even need to use serialization groups! We're going to go deeper into this later, but above the day property, we can hide this entirely from our API by using an #[Ignore] attribute from Symfony's serializer.
    
     If we head over here and "Execute" that... boom! That field is gone: it can't be read or written.

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -5,13 +5,14 @@ namespace App\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\State\DailyQuestStateProvider;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ApiResource(
 	shortName: 'Quest',
 	provider: DailyQuestStateProvider::class
 )]
 class DailyQuest {
-
+	#[Ignore]
 	public \DateTimeInterface $day;
 
 	public function __construct(\DateTimeInterface $day){
