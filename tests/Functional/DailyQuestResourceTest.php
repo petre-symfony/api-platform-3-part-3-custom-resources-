3c4780745b6c2fc12d22306ba227fdea88f6d8f6
Petre Ro <petrero@gmail.com>
2023-10-10 18:29:24 +0300

    11.1 Custom Resource State Processor
     We haven't configured the operations key on our #[ApiResource]. And so, we get every default operation. But we really only need a few. Add operations with a new GetCollection(), new Get() to fetch a single quest and new Patch() so users can update the status of an existing quest when they complete it.
    
     Upon refreshing... I love it!
    
     Speaking of that Patch operation, when it's used, API Platform will call the state processor, so we can save... or do whatever we want. We don't have one yet, so that'll be our next job.
    
     Adding a Patch Test
     - But let's start with a test. Down in tests/Functional/, create a new class called DailyQuestResourceTest. Make this extend the ApiTestCase that we created in the last tutorial and use ResetDatabase from Foundry to make sure our database is empty at the start of every test. Also use Factories.
    
     Ok, we don't need these... since we're not going to talk to the database... but if we decide to later on, we're ready.
    
     Down here, add public function testPatchCanUpdateStatus(). The first thing we need is a new \DateTime() that represents $yesterday: -1 day.
    
     Remember: in our provider, we're creating daily quests for today through the last 50 days. When we make a PATCH request, our item provider is called to load the object. So we need to use a date that we know will be found.
    
     Now say $this->browser(), ->patch()... and the URL: /api/quests/ with $yesterday->format('Y-m-d'). Pass a second options argument with json and an array with 'status' => 'completed'.
    
     The status field is an enum... but because it's backed by a string, the serializer will deserialize it from the string active or completed. Finish with ->assertStatus(200), ->dump() (that will be handy in a second), and then ->assertJsonMatches() to check that status changed to completed.
    
     Wonderful! We're not really going to save the updated status... but we should at least see that the final JSON has status completed. Copy this test name... and over here, run: symfony php bin/phpunit --filter= and paste that name:
    
      symfony php bin/phpunit --filter=testPatchCanUpdateStatus
     And... whoops! We get a 415. The error says:
    
      The content-type application/json is not supported.

---
+++ tests/Functional/DailyQuestResourceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class DailyQuestResourceTest extends ApiTestCase {
+	use ResetDatabase;
+	use Factories;
+
+	public function testPatchCanUpdateStatus() {
+		$yesterday = new \DateTime('-1 day');
+		$this->browser()
+			->patch('/api/quests/' . $yesterday->format('Y-m-d'), [
+				'json' => [
+					'status' => 'completed'
+				]
+			])
+			->assertStatus(200)
+			->dump()
+			->assertJsonMatches('status', 'completed')
+		;
+	}
+}
