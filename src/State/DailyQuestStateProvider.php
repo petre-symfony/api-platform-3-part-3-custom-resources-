518a8f25f94f5cd791dae3948e9632e82fa61011
Petre Ro <petrero@gmail.com>
2023-10-11 18:49:23 +0300

    14.2 Organizing our Variables  - To help us do that, let's set a few variables: $currentPage hardcoded to 1, $itemsPerPage hardcoded to 10 and $totalItems. For this, call a new private method countTotalQuests().
     I'll hit Alt+Enter and add that method at the bottom. This will return an int... and I'm just going to return 50...
    
     because that's the total possible quests we have in our "fake" database. If you were using a database, you'd count every available row. Change the code in createQuests() to use this.
    
     This probably looks a bit silly: why am I creating a private method to return something so simple? Well, what I really want to highlight are the two distinct "jobs" of pagination. First, to return the correct subset of the 50 results - which we'll do in a moment. Second, to return the count of the total number of items. When you use Doctrine, it executes 2 separate queries for this: one to fetch the current page's results with a LIMIT and OFFSET, and a second COUNT query to count every row.
    
     Current Page, Limit, Offset: The Pagination Service
     - Ok, back on top, let's use these variables: $currentPage, $itemsPerPage and $totalItems.
    
     Ok cool... but what we really need to do is determine the actual current page and then use that to return only a subset of the results. Like, if we're showing 10 per page... and we're on page 2, we should return quests 11 through 20.

--- src/State/DailyQuestStateProvider.php
+++ src/State/DailyQuestStateProvider.php
@@ -19,13 +19,17 @@ class DailyQuestStateProvider implements ProviderInterface {
 
 	public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
 		if ($operation instanceof CollectionOperationInterface) {
+			$currentPage =1;
+			$itemsPerPage = 10;
+			$totalItems = $this->countTotalQuests();
+
 			$quests =  $this->createQuests();
 
 			return new TraversablePaginator(
 				new \ArrayIterator($quests),
-				1,
-				10,
-				count($quests)
+				$currentPage,
+				$itemsPerPage,
+				$totalItems
 			);
 		}
 
@@ -37,7 +41,7 @@ class DailyQuestStateProvider implements ProviderInterface {
 	private function createQuests(): array {
 		$treasures = $this->treasureRepository->findBy([], [], 10);
 		$quests = [];
-		for ($i = 0; $i < 50; $i++) {
+		for ($i = 0; $i < $this->countTotalQuests(); $i++) {
 			$quest = new DailyQuest(new \DateTimeImmutable(sprintf('- %d days', $i)));
 			$quest->questName = sprintf('Quest %d', $i);
 			$quest->description = sprintf('Description %d', $i);
@@ -56,5 +60,9 @@ class DailyQuestStateProvider implements ProviderInterface {
 			$quests[$quest->getDayString()] = $quest;
 		}
 		return $quests;
+	}
+
+	private function countTotalQuests(): int{
+		return 50;
 	}
 }
