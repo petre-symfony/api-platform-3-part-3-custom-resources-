c339510b34bd77c7fcb6d350d2a72b4154a0bb84
Petre Ro <petrero@gmail.com>
2023-10-11 17:34:18 +0300

    13.1 Embedding Custom DTO's
     One goal of the daily quests resource is to showcase the bountiful treasures a dragon can win by completing a quest. Embedding an array of DragonTreasure objects and showing their IRIs is a nice way to do that! But it's not the only way.
    
     Creating the Custom (non-ApiResource) Class
     - Idea time: forget about pointing to the exact treasures. What if we simply render the name, cool factor, and value of each as a custom array of embedded data? Check it out. In the src/ApiResource/ directory, though this class could live anywhere, create a new class called DailyQuestTreasure. This will represent the treasure that you could win by completing a DailyQuest.
    
     Inside, create a public function __construct with a public string $name, public int $value and public int $coolFactor. I'm using public properties for simplicity and even including all three as arguments to the constructor to make life even easier.
    
     But, I am not going to make this an ApiResource. Well, we could do that... if we need our API users to be able to fetch DailyQuestTreasure data directly... or update them. But that's not the point of this class. This will simply be a data structure that we attach to DailyQuest.
    
     Over in DailyQuest, this will no longer hold an array of DragonTreasure objects: it will hold an array of QuestTreasure objects. Oh, actually, to keep things shorter... there we go... call it QuestTreasure... then over here, QuestTreasure.
    
     Now that we have the property set up, head to the provider to populate it. Instead of setting the random dragon treasures onto this directly, we need to create an array of QuestTreasure objects. For each over the random treasures as $treasure... then $questTreasures[] equals new QuestTreasure and pass in the data: $treasure->getName(), $treasure->getValue() and $treasure->getCoolFactor(). Finish with $quest->treasures = $questTreasures.
    
     "Relations" that are Normal Objects
     - Before and after this change, our DailyQuest class had a property that held an array of objects. The key difference is that, before, it held an array of objects that were API resources. But now, it holds an array of normal, boring objects that are not API resources.
    
     What difference does that make? Check it out. Boom! Embedded objects! When API Platform serializes the treasures property, it sees that our QuestTreasure is not an ApiResource. So it serializes it in the normal way: by embedding each property.
    
     This is beautifully simple. And it's something I want you to remember: you can always create new data classes if you want to embed some extra data.

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -7,7 +7,6 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
-use App\Entity\DragonTreasure;
 use App\Enum\DailyQuestStatusEnum;
 use App\State\DailyQuestStateProcessor;
 use App\State\DailyQuestStateProvider;
@@ -33,7 +32,7 @@ class DailyQuest {
 	public DailyQuestStatusEnum $status;
 	public \DateTimeInterface $lastUpdated;
 	/**
-	 * @var DragonTreasure[]
+	 * @var QuestTreasure[]
 	 */
 	public array $treasures;
 
---
+++ src/ApiResource/QuestTreasure.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\ApiResource;
+
+class QuestTreasure {
+	public function __construct(
+		public string $name,
+		public int $value,
+		public int $coolFactor
+	) {
+
+	}
+}
