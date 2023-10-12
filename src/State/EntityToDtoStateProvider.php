259c9cb89909c915edcbb5c75d8acfc231a34986
Petre Ro <petrero@gmail.com>
2023-10-12 21:01:20 +0300

    17.2 Ok! In EntityToDtoStateProvider, we could manually query for our User entity objects, turn those into UserApi objects... then return them. But that's the whole thing we're trying to avoid! We want to continue to reuse all of that nice Doctrine query logic: that's the beauty of stateOptions.
     To do that, like we've done before, we're going to decorate the core Doctrine provider. Say public function __construct() with private ProviderInterface $collectionProvider. And to help Symfony know which to pass in, use the #[Autowire()] attribute and say service: CollectionProvider (make sure you get the one from Doctrine ORM), followed by ::class.
    
     Down here, add $entities = $this->collectionProvider->provide(), passing $operation, $uriVariables, and $context. Below, dd($entities)
    
     Let's see what happens! Head back over, refresh the endpoint, and... got it! We are calling the core provider, and it's returning a paginator object.

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -2,11 +2,20 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EntityToDtoStateProvider implements ProviderInterface {
+	public function __construct(
+		#[Autowire(service: CollectionProvider::class)] private ProviderInterface $collectionProvider
+	) {
+
+	}
+
 	public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
-		// Retrieve the state from somewhere
+		$entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
+		dd($entities);
 	}
 }
