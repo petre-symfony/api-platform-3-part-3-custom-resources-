ec173f1d6ff4ee233f4e7883e9460cdcc3c2da1f
Petre Ro <petrero@gmail.com>
2023-10-12 19:50:27 +0300

    16.7 Built-in Pagination  - So far, we know that stateOptions does three things. One: It automatically sets the provider and processor to use the core Doctrine provider and processor. Two: the provider is smart enough to query from this entity. This also works for single items, like /users/1.jsonld. And three: The serializer serializes the User entity into a UserApi object.
     The fact that stateOptions causes the core Doctrine state provider to be used has some very important other side effects. First, we get pagination for free. Add paginationItemsPerPage: 5, go over, and refresh. We see that the total number of items is "11"... but it only shows five... and the pages are down here.
    
     Second, the collection provider also makes the query extension system work. We don't have any query extensions for User, but we do have one for DragonTreasure. Later on, when we convert DragonTreasure to its own DTO class, this extension is still going to work.
    
     The third and final goodie is that the filter system still works! Watch: above UserApi, add #[ApiFilter()] with SearchFilter::class and properties: with username set to partial.
    
     Go back and look at the documentation... whoops. I autocompleted the SearchFilter from ODM. Delete that, then I'll hit Alt+Enter to grab the one from ORM.
    
     Refresh the docs again... and look at the /api/users endpoint. It is advertising that there's a username filter, and it is going to work! In the other tab, add ?username=Clumsy.
    
     And... yes! It only returns those 5 results! So the filter system works! Though, one thing to note is that, when we say username, we're referring to the $username property on the User entity. As far as the filter is concerned, we don't even need a username in UserApi.
    
     So: we're reusing all of this core Doctrine provider logic, we have pagination, filters and.... it's the best thing since ice cream sandwiches. Except... for that big, scary limitation: that our DTO can't have custom fields. And... that's really the whole point of a DTO: to gain the flexibility of having different fields than your entity. So let's see how to fix that limitation next.

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,16 +2,22 @@
 
 namespace App\ApiResource;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\DragonTreasure;
 use App\Entity\User;
 
 #[ApiResource(
 	shortName: 'User',
+	paginationItemsPerPage: 5,
 	stateOptions: new Options(entityClass: User::class)
 )]
+#[ApiFilter(SearchFilter::class, properties: [
+	'username' => 'partial'
+])]
 class UserApi {
 	public ?int $id = null;
 
