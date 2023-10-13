9a5a43de4d8a94d90e83d92b281a205cf892cd21
Petre Ro <petrero@gmail.com>
2023-10-13 18:47:23 +0300

    18.1 Provider: Transforming Entities to DTOs
     Let's keep track of the goal. When we first used stateOptions, it triggered the core Doctrine collection provider to be used. That's great... except that it returns User entities, meaning that those became the central objects for the UserApi endpoints. That causes a serious limitation when serializing: our UserApi properties need to match our User properties... otherwise the serializer explodes.
    
     To fix that and give us full control, we've created our own state provider that calls the core collection provider. But instead of returning these User entity objects, we're going to return UserApi objects so that they become the central objects and serialize normally.
    
     Mapping to the DTO
     - Create a $dtos array and foreach over $entities as $entity. Then add to the $dtos array by calling a new method: mapEntityToDto($entity).
    
     Hit "alt" + "enter" to add that method at the bottom. This will return an object. Well... it will be a UserApi object... but we're trying to keep this class generic. I'll paste in some logic - you can copy this from the code block on this page - then hit "alt" + "enter" to add the missing use statement. This code is user-specific... but we'll make it more generic later, so we can reuse this class for dragon treasures.
    
     But isn't this refreshingly boring and understandable code? Just transferring properties from the User $entity... onto the DTO. The only thing that's kind of fancy is where we change this collection to an array... because this property is an array on UserApi.
    
     Finally, at the bottom of provide(), return $dtos.
    
     Thanks to this, the central objects will be UserApi objects... and these will be serialized normally: no fanciness where the serializer tries to go from a User entity into a UserApi.
    
     Drumoll please! Tada! It works... with the same result as before! But now we have the power to add custom properties.

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -5,6 +5,7 @@ namespace App\State;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\UserApi;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EntityToDtoStateProvider implements ProviderInterface {
@@ -16,6 +17,22 @@ class EntityToDtoStateProvider implements ProviderInterface {
 
 	public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
 		$entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
-		return $entities;
+
+		$dtos = [];
+		foreach ($entities as $entity) {
+			$dtos[] = $this->mapEntityToDto($entity);
+		}
+
+		return $dtos;
+	}
+
+	private function mapEntityToDto(object $entity): object {
+		$dto = new UserApi();
+		$dto->id = $entity->getId();
+		$dto->email = $entity->getEmail();
+		$dto->username = $entity->getUsername();
+		$dto->dragonTreasures = $entity->getDragonTreasures->toArray();
+
+		return $dto;
 	}
 }
