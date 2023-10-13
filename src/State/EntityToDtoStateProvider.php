ad3e249022d699e933fceea355b31c5508b97a86
Petre Ro <petrero@gmail.com>
2023-10-13 20:49:21 +0300

    19.1 Entity -> DTO Item State Provider
     What about the item endpoint? If we go to /api/users/6.jsonld... it looks like it works... but it's a trap! It's just the collection format... with a single item!
    
     We know that there are two core providers: CollectionProvider and an item provider, whose job is to return one item or null. Because we set provider to EntityToDtoStateProvider, it's using this one provider for every operation. And that's ok... as long as we make it smart enough to handle both cases.
    
     We saw how to do this earlier:  is the key. Add if ($operation instanceof CollectionOperationInterface). Now we can warp all of this code up here. Lovely!
    
     Below, this will be our item provider. dd($uriVariables).
    
     When we try the item operation... nice! That's what we expect to see: the id value, which is the dynamic part of the route.

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -4,6 +4,7 @@ namespace App\State;
 
 use ApiPlatform\Doctrine\Orm\Paginator;
 use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\Pagination\TraversablePaginator;
 use ApiPlatform\State\ProviderInterface;
@@ -18,20 +19,24 @@ class EntityToDtoStateProvider implements ProviderInterface {
 	}
 
 	public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null {
-		$entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
-		assert($entities instanceof Paginator);
+		if ($operation instanceof CollectionOperationInterface) {
+			$entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
+			assert($entities instanceof Paginator);
 
-		$dtos = [];
-		foreach ($entities as $entity) {
-			$dtos[] = $this->mapEntityToDto($entity);
+			$dtos = [];
+			foreach ($entities as $entity) {
+				$dtos[] = $this->mapEntityToDto($entity);
+			}
+
+			return new TraversablePaginator(
+				new \ArrayIterator($dtos),
+				$entities->getCurrentPage(),
+				$entities->getItemsPerPage(),
+				$entities->getTotalItems()
+			);
 		}
 
-		return new TraversablePaginator(
-			new \ArrayIterator($dtos),
-			$entities->getCurrentPage(),
-			$entities->getItemsPerPage(),
-			$entities->getTotalItems()
-		);
+		dd($uriVariables);
 	}
 
 	private function mapEntityToDto(object $entity): object {
