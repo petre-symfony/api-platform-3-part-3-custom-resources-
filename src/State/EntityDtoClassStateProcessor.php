ed28dddae812424a86038fae30c3360f0aa44f03
Petre Ro <petrero@gmail.com>
2023-11-01 12:49:44 +0200

    20.4 Our job in the state processor is simple but important: to convert this UserApi back to a User entity so that we can save it. Say assert($data instanceof UserApi) and, inside, $entity = set to a new helper function: $this->mapDtoToEntity($data). Below, dd($entity).
     Then go add that new private function mapDtoToEntity(), which will accept an object $dto argument and return another object.
    
     Again, we know this will really accept a UserApi object and return a User entity... but we're trying to keep this class generic so we can reuse it later. Though we are going to have some user-specific code down here temporarily. In fact, to help our editor, add another assert($dto instanceof UserApi).
    
     Querying for the Existing Entity
     - We need to think about two different cases. The first is when we POST to create a brand-new user. In that case, $dto will have a null id. And that means we should create a fresh User object. The other case is if we were making, for example, a PATCH request to edit a user. In that case, the item provider will first load that User entity from the database... our provider will turn that into a UserApi object with id equal to 6... and that will eventually be passed to us here. If the id is 6... we don't want to create a new User object: we want to query the database for tha existing User. Our job is to handle both situations.
    
     Now, if $dto->id, we need to query for an existing User. To do that, on top, add a constructor with private UserRepository $userRepository.
    
     Back down here, say $entity = $this->userRepository->find($dto->id).
    
     If we don't find that User, throw a big giant exception that will trigger a 500 error with Entity %d not found.
    
     You might be wondering:
    
      Shouldn't this trigger a 404 error instead?
    
     The answer, in this case, is no. If we're in this situation, it means the item state provider has already successfully queried for a User with this id. So there should be no way for us to suddenly not find it. There are some exceptions to this, like if you allowed your user to change their id... or if you allowed users to create brand-new objects and set the id manually... but for most situations, including ours, if this happens, something went weird.
    
     Next up, if we don't have an id, say $entity = new User().
    
     Done! In both cases, down here, we're going to map the $dto object to the $entity object. This code is boring... so I'll speed through this. For the password, put a TODO temporarily because we still need to hash that. Also add a TODO for handle dragon treasures. Just focus on the easy stuff... and at the bottom, return $entity.
    
     If we've done things correctly, we'll take the UserApi, transform that into an $entity and dump it. Rerun the test:
    
      symfony php bin/phpunit --filter=testPostToCreateUser
     And... 404! Let's see what happened here. Oh... of course. I never put my test back together. This should be ->post('/api/users'). Try that again and... got it! There's our User entity object with the email and username transferred correctly!
    
     Next: Let's save this by leveraging the core Doctrine PersistProcessor and RemoveProcessor. We'll also handle hashing the password. By the end, our user tests will be passing with flying colors.

--- src/State/EntityDtoClassStateProcessor.php
+++ src/State/EntityDtoClassStateProcessor.php
@@ -4,9 +4,42 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\UserApi;
+use App\Entity\User;
+use App\Repository\UserRepository;
 
 class EntityDtoClassStateProcessor implements ProcessorInterface {
+	public function __construct(
+		private UserRepository $userRepository
+	) {
+
+	}
 	public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void {
-		dd($data);
+		assert($data instanceof UserApi);
+
+		$entity = $this->mapDtoToEntityData($data);
+
+		dd($entity);
+	}
+
+	private function mapDtoToEntityData(object $dto): object {
+		assert($dto instanceof UserApi);
+
+		if ($dto->id) {
+			$entity = $this->userRepository->find($dto->id);
+
+			if (!$entity) {
+				throw new \Exception(sprintf('Entity %d not found', $dto->id));
+			}
+		} else {
+			$entity = new User();
+		}
+
+		$entity->setEmail($dto->email);
+		$entity->setUsername($dto->username);
+		$entity->setPassword('TODO properly');
+		//TODO: handle dragon treasures
+
+		return $entity;
 	}
 }
